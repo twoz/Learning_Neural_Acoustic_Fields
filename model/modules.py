78e0bbf8360fc3cf9dfebc3dd6ec36ae17989bee
Tomasz Wozniak <twozniak@magicleap.com>
2023-01-09 10:36:45 +0100

    model modifications to allow saving to TorchScript

--- model/modules.py
+++ model/modules.py
@@ -22,6 +22,25 @@ class embedding_module_log(nn.Module):
                 out_list.append(func(x_input*freq))
         return torch.cat(out_list, dim=self.ch_dim)
 
+# Version of the `embedding_module_log` with sin & cos functions hardcoded
+class sin_cos_embedder(nn.Module):
+    def __init__(self, num_freqs=20, max_freq=10, ch_dim=1, include_in=True):
+        super().__init__()
+        self.freqs = torch.nn.Parameter(2.0**torch.from_numpy(np.linspace(start=0.0,stop=max_freq, num=num_freqs).astype(np.single)), requires_grad=False)
+        self.ch_dim = ch_dim
+        self.include_in = include_in
+
+    def forward(self, x_input):
+        if self.include_in:
+            out_list = [x_input]
+        else:
+            out_list = []
+        for freq in self.freqs:
+            out_list.append(torch.sin(x_input*freq))
+        for freq in self.freqs:
+            out_list.append(torch.cos(x_input*freq))
+        return torch.cat(out_list, dim=self.ch_dim)
+
 def distance(x1, x2):
     # by jacobrgardner
     x1_norm = x1.pow(2).sum(dim=-1, keepdim=True)
@@ -30,7 +49,7 @@ def distance(x1, x2):
     return res
 
 # small improvement in performance
-# @torch.jit.script
+@torch.jit.script
 def fit_predict_torch(input_pos:torch.Tensor, input_target:torch.Tensor, predict_pos:torch.Tensor, bandwidth:torch.Tensor) -> torch.Tensor:
     dist_vector = -distance(predict_pos, input_pos)
     gauss_dist = torch.exp(dist_vector/(2.0 * torch.square(bandwidth.unsqueeze(0))))
